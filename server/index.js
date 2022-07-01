f5b6dc7213ddcdb5b9f9a0c1760db7729fbc0f9e
johnpaul <edehjohnpaul@gmail.com>
2022-07-01 09:44:10 -0700

    login

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
 const jwt = require('jsonwebtoken')
+const bcrypt = require('bcryptjs')
 const User = require('./models/user')
 
 app.use(cors())
@@ -11,26 +12,31 @@ app.use(express.json())
 app.post('/api/register', async (req, res) => {
     console.log(req.body)
     try {
-        const user = await User.create({
+        const newPassword = await bcrypt.hash(req.body.password, 10)
+        await User.create({
             name: req.body.name,
             email: req.body.email,
-            password: req.body.password,
+            password: newPassword,
             location: req.body.location,
         })
         res.json({status:'ok'})
     }catch (err){
-        console.log(err)
         res.json({status:'error', error: 'duplicate email'})
     }
 })
 app.post('/api/login', async (req, res) => {
     const user = await User.findOne({
         email: req.body.email,
-        password: req.body.password,
     })
 
+    if (!user) {
+        return{ status:'error', error:'invalid login'}
+    }
 
-    if (user) {
+    const isPasswordValid = await bcrypt.compare(req.body.password, user.password)
+
+
+    if (isPasswordValid) {
 
         const token = jwt.sign(
             {
